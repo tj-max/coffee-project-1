68c78ebddc38836a1f0eb00cc90276e8cdd15000
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-06-26 12:10:49 -0500

    Added search functionality, added func to populated div

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+    var html = '<h3>' + coffee.name + '</h3>';
+    html += '<p>' + coffee.roast + '</p>';
 
     return html;
 }
 
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    for(var i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
@@ -21,14 +18,24 @@ function renderCoffees(coffees) {
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
+    var searched = coffeeSearch.value;
     var filteredCoffees = [];
+
     coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
+
+        if (coffee.roast === selectedRoast || selectedRoast === 'all') {
+            if (coffee.name === searched) {
+                filteredCoffees.push(coffee);
+                console.log('comparison passed.');
+            } else if (searched === '') {
+                filteredCoffees.push(coffee);
+            }
         }
+
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+    divCoffee.innerHTML = renderCoffees(filteredCoffees);
 }
+
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
@@ -48,10 +55,11 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
-var tbody = document.querySelector('#coffees');
+var divCoffee = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
+var coffeeSearch = document.querySelector('#coffee-search');
 
-tbody.innerHTML = renderCoffees(coffees);
+divCoffee.innerHTML = renderCoffees(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
+console.log(submitButton.addEventListener('click', updateCoffees));
